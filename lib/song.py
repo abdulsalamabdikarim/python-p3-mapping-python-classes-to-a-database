f03fdb5d091933f83ec3bfda8600701dfa083f38
Abdulsalam Abdikarim <achalam00@gmail.com>
2023-12-09 19:07:57 +0300

    Completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,13 +1,14 @@
 from config import CONN, CURSOR
 
 class Song:
+
     def __init__(self, name, album):
         self.id = None
         self.name = name
         self.album = album
 
     @classmethod
-    def create_table(self):
+    def create_table(cls):
         sql = """
             CREATE TABLE IF NOT EXISTS songs (
                 id INTEGER PRIMARY KEY,
@@ -25,6 +26,8 @@ class Song:
         """
 
         CURSOR.execute(sql, (self.name, self.album))
+        CONN.commit()
+
         self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
 
     @classmethod
